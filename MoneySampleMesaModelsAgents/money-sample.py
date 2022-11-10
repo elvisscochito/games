64573e5aea7376385d8c830a87e1e0aa78304ace
Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>
2022-11-10 08:01:21 -0600

    Add training function
    To repeat model over 100 times and 10 step by each one

--- MoneySampleMesaModelsAgents/money-sample.py
+++ MoneySampleMesaModelsAgents/money-sample.py
@@ -34,19 +34,48 @@ class MoneyModel (mesa.Model):
         # Advance the model by one step (method build in scheduler)
         self.schedule.step()
 
+final_wealth = []
+
+def training():
+
+    # Run the model 100 times, each model executing 10 steps
+    for j in range(100):
+        # Create the model
+        model = MoneyModel(10)
+
+        # Print the model's current wealth
+        for agent in model.schedule.agents:
+            print(f"Agent {agent.unique_id} starts with wealth {agent.wealth}")
+
+        # Advance the model by 10 steps
+        for i in range(10):
+            # Run the model
+            model.step()
+
+        for agent in model.schedule.agents:
+            # print the final wealth
+            print(f"Agent {agent.unique_id} ends with wealth {agent.wealth}")
+            # Store the model's wealth
+            final_wealth.append(agent.wealth)
+
+    plot_model(model)
+
 def plot_model(model):
     # Plot the number of agents in the model
-    agent_wealth = [agent.wealth for agent in model.schedule.agents]
+    """ agent_wealth = [agent.wealth for agent in model.schedule.agents]
     plt.hist(agent_wealth)
+    plt.title("Wealth distribution")
+    plt.xlabel("Wealth")
+    plt.ylabel("Number of agents")
+    plt.show() """
+
+    # Plot the model's final wealth distribution
+    plt.hist(final_wealth, bins=range(max(final_wealth)+1))
     plt.title("Wealth distribution")
     plt.xlabel("Wealth")
     plt.ylabel("Number of agents")
     plt.show()
 
 if __name__ == "__main__":
-    # Create a model
-    empty_model = MoneyModel(10)
     # Run the model
-    empty_model.step()
-    # Plot the model
-    plot_model(empty_model)
+    training()
